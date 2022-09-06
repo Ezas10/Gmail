781cd8dae11f1b69f428049d437a71115304d8fc
Ezas10 <shaikezas10@gmail.com>
2022-09-06 17:49:01 +0530

    Second commit

---
+++ Gmail/src/Gmail.java
@@ -0,0 +1,18 @@
+public class Gmail {
+public static void main(String[]args) {
+	String email = "shaikezas0@gmail.com";
+	if(email == "shaikezas10@gmail.com") {
+	System.out.println("email is shaikezas10@gmail.com");
+	String gmail= "shaikezas10@gmail.com";
+}else if(email=="ezas7862@gmail.com") {
+	System.out.println("email is ezas7862@gmail.com ");
+	String gmail = "ezas7862@gmail.com";
+}else if(email=="ezas78623@gmail.com") {
+	System.out.println("email is ezas78623@gmail.com");
+	String gmail= "ezas78623@gmail.com";
+}else {
+	System.out.println(email);
+}
+}
+}
+
